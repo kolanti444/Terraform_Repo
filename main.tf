8ce1ecc5cb80d6bf6992bcf44cba53d1babdd095
kolanti444 <84433091+kolanti444@users.noreply.github.com>
2023-02-06 15:19:35 +0530

    Added man.tf file

---
+++ main.tf
@@ -0,0 +1,60 @@
+provider "aws" {
+	access_key = "${var.aws_access_key}"
+	secret_key = "${var.aws_secret_key}"
+	region     = "${var.aws_region}"
+}
+
+resource "aws_vpc" "myfirstVPC" {
+	cidr_block = "${var.vpc_cidr}"
+	enable_dns_hostnames = true
+	tags = {
+		name = "${var.vpc_name}"
+	}
+}
+
+resource "aws_subnet" "publicsubnet1" {
+	cidr_block = "${var.public_subnet1_cidr}"
+	vpc_id = "${aws_vpc.myfirstVPC.id}"
+	availability_zone = "us-east-1a"
+	
+	tags = {
+		name = "${var.public_subnet1_name}"
+	}
+}
+
+resource "aws_subnet" "publicsubnet2" {
+	cidr_block = "${var.public_subnet2_cidr}"
+	vpc_id = "${aws_vpc.myfirstVPC.id}"
+	availability_zone = "us-east-1b"
+	
+	tags = {
+		name = "${var.public_subnet2_name}"
+	}
+}
+
+resource "aws_subnet" "privatesubnet1" {
+	cidr_block = "${var.private_subnet1_cidr}"
+	vpc_id = "${aws_vpc.myfirstVPC.id}"
+	availability_zone = "us-east-1c"
+	
+	tags = {
+		name = "${var.private_subnet1_name}"
+	}
+}
+resource "aws_subnet" "privatesubnet2" {
+	cidr_block = "${var.private_subnet2_cidr}"
+	vpc_id = "${aws_vpc.myfirstVPC.id}"
+	availability_zone = "us-east-1e"
+	
+	
+	tags = {
+		name = "${var.private_subnet2_name}"
+	}
+}
+
+resource "aws_internet_gateway" "default"{
+	vpc_id = "${aws_vpc.myfirstVPC.id}"
+	tags = {
+		name = "${var.IGW_name}"
+	}
+}
